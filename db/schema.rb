f1e86a21b1ea212e51ecc4f2b8e7ebd79cc958a7
Bartosz Krajka <krajka.bartosz@gmail.com>
2025-06-16 16:39:14 +0200

    Bare `rails g authentication`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_25_112217) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_16_143750) do
   create_table "array_nodes", force: :cascade do |t|
     t.string "value_type", null: false
     t.integer "value_id", null: false
@@ -70,6 +70,23 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_25_112217) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
   create_table "versions", force: :cascade do |t|
     t.string "name", null: false
     t.integer "order", null: false
@@ -81,5 +98,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_25_112217) do
 
   add_foreign_key "endpoints", "versions"
   add_foreign_key "object_attributes", "object_nodes", column: "parent_id"
+  add_foreign_key "sessions", "users"
   add_foreign_key "versions", "projects"
 end
