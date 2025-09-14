ee72a2430797e97beff4cb1995e565377de993ea
Bartosz Krajka <krajka.bartosz@gmail.com>
2025-09-14 14:42:58 +0200

    Add custom response type to test server

--- app/controllers/test_server_controller.rb
+++ app/controllers/test_server_controller.rb
@@ -2,14 +2,14 @@ class TestServerController < ApplicationController
   skip_before_action :verify_authenticity_token
   allow_unauthenticated_access
 
+  class InvalidResponseCode < StandardError; end
+
   def version
     project = Project.find_by!(name: request.params[:project_name])
     version = Version.find_by!(project: project, name: request.params[:version_name])
     # authorize
     endpoint = Endpoint.from_version_request(request, version).first
-    output = endpoint.parsed_output.to_example_json
-
-    render json: output
+    render json: output(endpoint, request).to_example_json
   end
 
   def candidate
@@ -18,8 +18,17 @@ class TestServerController < ApplicationController
     # authorize
     version = candidate.latest_version
     endpoint = Endpoint.from_candidate_request(request, version).first
-    output = endpoint.parsed_output.to_example_json
+    render json: output(endpoint, request).to_example_json
+  end
 
-    render json: output
+  def output(endpoint, request)
+    desired_response = request.params[:response]
+    return endpoint.parsed_output if desired_response.nil?
+
+    response = endpoint.responses.where(code: desired_response)
+    raise InvalidResponseCode.new("Invalid response code: #{desired_response}") if response.empty?
+
+    return endpoint.parsed_output if desired_response.start_with?("2")
+    endpoint.parsed_output_error
   end
 end
